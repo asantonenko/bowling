e9cabfb2382d7aff0ddbd94fffc526ad5e592767
Alexander Antonenko <asantonenko@gmail.com>
2023-06-08 13:55:49 +0300

    Завантажена реалізація класа Frame

---
+++ bowling/Frame.cpp
@@ -0,0 +1,76 @@
+#include "Frame.h"
+
+bool Frame::IsCorrectRolls(int firstRoll, int secondRoll)
+{
+    if (firstRoll == NO_ROLL && secondRoll == NO_ROLL)
+    {
+        return true;
+    }
+
+    if (firstRoll <= TOTAL_PINS && secondRoll == NO_ROLL)
+    {
+        return true;
+    }
+
+    if (firstRoll != NO_ROLL && secondRoll != NO_ROLL && firstRoll + secondRoll <= TOTAL_PINS)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+void Frame::SetFirstRoll(int val)
+{
+    if (m_FirstRoll != NO_ROLL)
+    {
+        throw std::logic_error("Roll is already done");
+    }
+    if (!IsCorrectRolls(val, NO_ROLL))
+    {
+        throw std::invalid_argument("Incorrect rolls");
+    }
+    m_FirstRoll = val;
+}
+
+void Frame::SetSecondRoll(int val)
+{
+    if (m_SecondRoll != NO_ROLL)
+    {
+        throw std::logic_error("Roll is already done");
+    }
+    if (m_FirstRoll == NO_ROLL)
+    {
+        throw std::logic_error("You should make first roll before second roll");
+    }
+    if (!IsCorrectRolls(m_FirstRoll, val))
+    {
+        throw std::invalid_argument("Incorrect rolls");
+    }
+    m_SecondRoll = val;
+}
+
+int Frame::TakeAdditinalRolls(int number) const
+{
+    if (m_NextFrames[0] != nullptr)
+    {
+        if (number == 2 && m_NextFrames[0].CountRolls() == 2)
+        {
+            return m_NextFrames[0].GetFirstRoll() + m_NextFrames[0].GetSecondRoll();
+        }
+        if (number == 1 && m_NextFrames[0].CountRolls() == 1)
+        {
+            return m_NextFrames[0].GetFirstRoll();
+        }
+        if (number == 2 && m_NextFrames[0].CountRolls() == 1)
+        {
+            int sum = m_NextFrames[0].GetFirstRoll();
+            if (m_NextFrames[1] != nullptr && m_NextFrames[1].CountRolls() >= 1)
+            {
+                sum += m_NextFrames[1].GetFirstRoll();
+            }
+            return sum;
+        }
+    }
+    return 0;
+}
---
+++ bowling/Frame.h
@@ -0,0 +1,86 @@
+#ifndef FRAME_H
+#define FRAME_H
+
+#include<stdexcept>
+
+const int TOTAL_PINS = 10;
+const int NO_ROLL = -1;
+
+class Frame
+{
+    public:
+        Frame(int firstRoll = NO_ROLL, int secondRoll = NO_ROLL) : m_FirstRoll(firstRoll), m_SecondRoll (secondRoll)
+        {
+            if (!IsCorrectRolls(firstRoll, secondRoll))
+            {
+                throw std::invalid_argument("Incorrect rolls");
+            }
+        }
+        virtual ~Frame(){}
+
+        int GetFirstRoll() const{ return m_FirstRoll; }
+        void SetFirstRoll(int val);
+        int GetSecondRoll() const { return m_SecondRoll; }
+        void SetSecondRoll(int val);
+
+        bool isStrike() const
+        {
+            return m_FirstRoll == TOTAL_PINS;
+        }
+
+        bool isSpare() const
+        {
+            return !isStrike() && m_FirstRoll + m_SecondRoll == TOTAL_PINS;
+        }
+
+        int CountRolls() const
+        {
+            if (m_FirstRoll == NO_ROLL)
+            {
+                return 0;
+            }
+            if (m_SecondRoll == NO_ROLL)
+            {
+                return 1;
+            }
+            return 2;
+        }
+
+        int Sum() const
+        {
+            int result = 0;
+            switch(CountRolls())
+            {
+                case 2: result = m_FirstRoll + m_SecondRoll; break;
+                case 1: result = m_FirstRoll;
+            }
+            if (isStrike())
+            {
+                result += TakeAdditinalRolls(2);
+            }
+            else if (isSpare())
+            {
+                result += TakeAdditinalRolls(1);
+            }
+            return result;
+        }
+
+        bool IsFrameFinished() const
+        {
+            return (CountRolls() == 2) || (CountRolls() == 1 && isStrike());
+        }
+
+    protected:
+
+    private:
+        int m_FirstRoll;
+        int m_SecondRoll;
+
+        Frame* m_NextFrames[2];
+
+        static bool IsCorrectRolls(int firstRoll, int secondRoll); // Чи може бути два кмдки firstRoll secondRoll
+
+        int TakeAdditinalRolls(int number) const;
+};
+
+#endif // FRAME_H
